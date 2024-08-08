03ac2c5ecef3bc7c641e394f1c07a3ed3fef0181
Berkay SARICA <berkaysarica2005@gmail.com>
2024-08-08 21:39:20 +0300

    Adding backend of register site

--- index.js
+++ index.js
@@ -2,12 +2,27 @@ import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 import ejs from "ejs";
+import env from "dotenv";
+import bcrypt from "bcrypt";
+
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+
 const app = express();
 const port = 3000;
+const saltRounds = 12;
+env.config();
+
+const db = new pg.Client({
+    user: process.env.USER_NAME,
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    password: process.env.DBPASSWORD,
+    port: process.env.DBPORT,
+});
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -19,6 +34,30 @@ app.get("/", (req, res) => {
 app.get("/register", (req, res) => {
     res.sendFile(__dirname + "/public/register.html");
 });
+
+app.post("/register", async (req, res) => {
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    try {
+        const emailCheck = await db.query("SELECT * FROM users WHERE email = $1", [email]);
+        if (emailCheck.rows.length > 0) {
+            res.send("Email already exists!");
+        } else {
+            bcrypt.hash(password, saltRounds, async (err, hash) => {
+                if(err) {
+                    console.log("Some errors while hashing: " + err);
+                } else {
+                    await db.query("INSERT INTO users (username, email, password) VALUES ($1 ,$2 ,$3)", [username, email, hash]);
+                    res.send("Register başarılı");
+                }
+            })
+        }
+    } catch (error) {
+        console.log(err);
+    }
+})
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
