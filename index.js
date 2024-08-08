0b3584c338549ba160ab12a68ec50cfa21bba37b
Berkay SARICA <berkaysarica2005@gmail.com>
2024-08-08 19:09:22 +0300

    Adding a basic register page

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 import ejs from "ejs";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
@@ -9,6 +12,14 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+    res.redirect("/register");
+});
+
+app.get("/register", (req, res) => {
+    res.sendFile(__dirname + "/public/register.html");
+});
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
-  });
+});
