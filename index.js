ce31dfa469f81cee89d9e00b0f50e356ad506fc0
Berkay SARICA <berkaysarica2005@gmail.com>
2024-08-08 17:28:36 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+import ejs from "ejs";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
