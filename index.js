0d6b6dbad356d114e9cf83a55644ca7bf44d007d
Berkay SARICA <berkaysarica2005@gmail.com>
2024-08-09 12:30:12 +0300

    Adding the backend of the login page.

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ const port = 3000;
 const saltRounds = 12;
 env.config();
 
+let user = "";
+
 const db = new pg.Client({
     user: process.env.USER_NAME,
     host: process.env.HOST,
@@ -57,11 +59,42 @@ app.post("/register", async (req, res) => {
     } catch (error) {
         console.log(err);
     }
-})
+});
 
 app.get("/login", (req, res) => {
     res.sendFile(__dirname + "/public/login.html");
-})
+});
+
+app.post("/login", async(req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    try {
+        const emailCheck = await db.query("SELECT * FROM users WHERE email = $1", [email]);
+        if (emailCheck.rows.length === 0) {
+            res.send("User does not exist. Please register!");
+        } else {
+            const result = await db.query("SELECT password FROM users WHERE email = $1", [email]);
+            const hash = result.rows[0].password;
+            console.log("Hash: " + hash);
+            console.log("Password: " + password);
+            
+            bcrypt.compare(password, hash, (err, result) => {
+                if(err) {
+                    console.log("Error while comparing passwords: " + err);
+                } else {
+                    if(result) {
+                        res.send("Login başarılı");
+                    } else {
+                        res.send("Yanlış şifre girildi.");
+                    }
+                }
+            })
+        }
+    } catch (err) {
+        console.log("Some server side error: " + err);
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
